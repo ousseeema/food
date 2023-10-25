099d8b15160594095e099746e97cb5ddd7732cae
ousseeema <oussama.ferchichi01@gmail.com>
2023-10-25 11:08:05 +0100

    save address  btn

---
+++ lib/view/widgets/save_address_btt.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:food/view/utils/dimensions.dart';
+
+class SaveButt extends StatelessWidget {
+  final VoidCallback? OnPressed;
+  final String bttText;
+  final bool transparent;
+  final EdgeInsets? margin;
+  final double? height;
+  final double? width;
+  final double radius;
+  final IconData? icon;
+
+  const SaveButt(
+      {super.key,
+      this.OnPressed,
+      required this.bttText,
+      this.transparent = false,
+      this.margin,
+      this.height,
+      this.width,
+      this.radius = 5,
+      this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+   final ButtonStyle _floatbutton=  TextButton.styleFrom(
+      backgroundColor: OnPressed == null
+          ? Theme.of(context).disabledColor
+          : transparent
+              ? Colors.transparent
+              : Theme.of(context).primaryColor,
+      minimumSize: Size(width == null ? width! : Dimensions.screenwidth,
+          height == null ? height! : 50),
+          padding: EdgeInsets.zero,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(radius)),
+
+          
+    );
+    return  Center(
+
+      child: SizedBox(
+        width:  width?? Dimensions.screenwidth,
+        height: height?? 50,
+        child: TextButton(
+          onPressed: () {
+            
+          },
+          style: _floatbutton,
+          child:  Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            icon!= null ? Padding(
+              
+              
+              padding: EdgeInsets.only(right: Dimensions.width10/2 )
+              child: Icon(icon, color: transparent?Theme.of(context).primaryColor:Theme.of(context).cardColor ,),
+              
+              
+              ): SizedBox(),
+              Text(bttText, style: TextStyle(fontSize: Dimensions.font16, 
+              color: transparent?Theme.of(context).primaryColor:Theme.of(context).cardColor ,
+              ),) 
+
+            ],
+          )),
+
+      ),
+    );
+  }
+}
